225eb1971ebf81a6c3313a822976eba132a18c93
Hanyuan Zhao <hanyuan-z@qq.com>
2024-01-31 20:12:43 +0800

    test: add testbench for address validation

--- test/uart_8250_tst.v
+++ test/uart_8250_tst.v
@@ -1,58 +1,51 @@
 `timescale 1ns/1ps
 module uart_8250_tst();
     
-    reg [3:0] A;
-    reg [3:0] B;
-    reg CI;
-    wire [3:0] SUM;
-    wire [4:0] Carry;
+    reg CLK_O;
+    reg RST_O;
+    reg [31:0] ADR_O;
+    reg [31:0] DAT_O;
+    wire [31:0] DAT_I;
+    reg WE_O;
+    reg [3:0] SEL_O;
+    reg STB_O;
+    wire ACK_I;
+    reg CYC_O;
+    wire INT_I;
     
-    integer tst_cnt_1;
-    integer tst_cnt_2;
+    uart_8250 u(
+    CLK_O,
+    RST_O,
+    ADR_O,
+    DAT_O,
+    DAT_I,
+    WE_O,
+    SEL_O,
+    STB_O,
+    ACK_I,
+    CYC_O,
+    INT_I
+    );
     
-    uart_8250 fadd0(A[0],
-    B[0],
-    CI,
-    SUM[0],
-    Carry[1]);
-    
-    uart_8250 fadd2(A[1],
-    B[1],
-    Carry[1],
-    SUM[1],
-    Carry[2]);
-    
-    uart_8250 fadd3(A[2],
-    B[2],
-    Carry[2],
-    SUM[2],
-    Carry[3]);
-    
-    uart_8250 fadd4(A[3],
-    B[3],
-    Carry[3],
-    SUM[3],
-    Carry[4]);
-    
-    /*iverilog */
+    /* iverilog */
     initial
     begin
         $dumpfile("wave.vcd");        //生成的vcd文件名称
         $dumpvars(0, uart_8250_tst);     //tb模块名称
     end
-    /*iverilog */
+    /* iverilog */
     
     initial
     begin
-        CI = 0;
-        for(tst_cnt_1 = 0;tst_cnt_1<16;tst_cnt_1 = tst_cnt_1 +1)
-        begin
-            for(tst_cnt_2 = 0;tst_cnt_2<16;tst_cnt_2 = tst_cnt_2 +1)
-            begin
-                #1 A = tst_cnt_1;
-                B    = tst_cnt_2;
-            end
-        end
+        ADR_O <= 32'h1250_0000;
+        #1 ADR_O <= 32'h1250_0001;
+        #1 ADR_O <= 32'h1250_0002;
+        #1 ADR_O <= 32'h1250_0003;
+        #1 ADR_O <= 32'h1250_0010;
+        #1 ADR_O <= 32'h1250_00a0;
+        #1 ADR_O <= 32'h1256_0000;
+        #1 ADR_O <= 32'h0250_0000;
+        #1 ADR_O <= 32'hf250_0000;
         
         #1 $finish;
     end
