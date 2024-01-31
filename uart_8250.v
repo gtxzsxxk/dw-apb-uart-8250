c8c50d64f7cd9f6846a4813f3b608c245d332caa
Hanyuan Zhao <hanyuan-z@qq.com>
2024-01-31 21:42:25 +0800

    top: let the logic driven by CLK_I signal rather than CYC_I
    If there're more than two continuous operations, then
    the CYC_I would give no posedge. So we should still
    use CLK_I as the clock according to the wishbone's
    pipeline design

--- uart_8250.v
+++ uart_8250.v
@@ -32,7 +32,7 @@ module uart_8250 (input CLK_I,             /* 时钟 */
     reg [7:0] rx_fifo_head;
     reg [7:0] rx_fifo_tail;
     
-    always @(posedge CYC_I or negedge RST_I) begin
+    always @(posedge CLK_I or negedge RST_I) begin
         if (!RST_I) begin
             RHR <= 8'b0;
             IER <= 8'h0;
@@ -53,91 +53,98 @@ module uart_8250 (input CLK_I,             /* 时钟 */
             INT_O <= 1'b0;
         end
         else begin
-            case (offset)
-                4'h0: begin
-                    if (WE_I) begin
-                        /* TODO: 添加进fifo */
+            if (STB_I && CYC_I) begin
+                case (offset)
+                    4'h0: begin
+                        if (WE_I) begin
+                            /* TODO: 添加进fifo */
+                            DAT_O <= 32'bz;
+                        end
+                        else begin
+                            DAT_O <= {24'b0, RHR};
+                        end
+                        ACK_O <= 1'b1;
+                        INT_O <= 1'b0;
+                    end
+                    4'h1: begin
+                        if (WE_I) begin
+                            IER   <= DAT_I[7:0];
+                            DAT_O <= 32'bz;
+                        end
+                        else begin
+                            DAT_O <= {24'b0, IER};
+                        end
+                        ACK_O <= 1'b1;
+                        INT_O <= 1'b0;
+                    end
+                    4'h2: begin
+                        /* Write: FIFO Control */
+                        /* Read: Get Interrupt Information */
+                        if (WE_I) begin
+                            FCR   <= DAT_I[7:0];
+                            DAT_O <= 32'bz;
+                        end
+                        else begin
+                            DAT_O <= {24'b0, IIR};
+                        end
+                        ACK_O <= 1'b1;
+                        INT_O <= 1'b0;
+                    end
+                    4'h3: begin
+                        /* Line Control Register */
+                        if (WE_I) begin
+                            LCR   <= DAT_I[7:0];
+                            DAT_O <= 32'bz;
+                        end
+                        else begin
+                            DAT_O <= {24'b0, LCR};
+                        end
+                        ACK_O <= 1'b1;
+                        INT_O <= 1'b0;
+                    end
+                    4'h4: begin
+                        /* Modem Control Write Only */
+                        if (WE_I) begin
+                            LCR <= DAT_I[7:0];
+                        end
                         DAT_O <= 32'bz;
+                        ACK_O <= 1'b1;
+                        INT_O <= 1'b0;
                     end
-                    else begin
-                        DAT_O <= {24'b0, RHR};
+                    4'h5: begin
+                        /* Line Status Read Only */
+                        if (!WE_I) begin
+                            DAT_O <= {24'b0, LSR};
+                        end
+                        else begin
+                            DAT_O <= 32'bz;
+                        end
+                        ACK_O <= 1'b1;
+                        INT_O <= 1'b0;
                     end
-                    ACK_O <= 1'b1;
-                    INT_O <= 1'b0;
-                end
-                4'h1: begin
-                    if (WE_I) begin
-                        IER   <= DAT_I[7:0];
+                    4'h6: begin
+                        /* Line Status Read Only */
+                        if (!WE_I) begin
+                            DAT_O <= {24'b0, MSR};
+                        end
+                        else begin
+                            DAT_O <= 32'bz;
+                        end
+                        ACK_O <= 1'b1;
+                        INT_O <= 1'b0;
+                    end
+                    default: begin
                         DAT_O <= 32'bz;
+                        ACK_O <= 1'bz;
+                        INT_O <= 1'b0;
                     end
-                    else begin
-                        DAT_O <= {24'b0, IER};
-                    end
-                    ACK_O <= 1'b1;
-                    INT_O <= 1'b0;
-                end
-                4'h2: begin
-                    /* Write: FIFO Control */
-                    /* Read: Get Interrupt Information */
-                    if (WE_I) begin
-                        FCR   <= DAT_I[7:0];
-                        DAT_O <= 32'bz;
-                    end
-                    else begin
-                        DAT_O <= {24'b0, IIR};
-                    end
-                    ACK_O <= 1'b1;
-                    INT_O <= 1'b0;
-                end
-                4'h3: begin
-                    /* Line Control Register */
-                    if (WE_I) begin
-                        LCR   <= DAT_I[7:0];
-                        DAT_O <= 32'bz;
-                    end
-                    else begin
-                        DAT_O <= {24'b0, LCR};
-                    end
-                    ACK_O <= 1'b1;
-                    INT_O <= 1'b0;
-                end
-                4'h4: begin
-                    /* Modem Control Write Only */
-                    if (WE_I) begin
-                        LCR <= DAT_I[7:0];
-                    end
-                    DAT_O <= 32'bz;
-                    ACK_O <= 1'b1;
-                    INT_O <= 1'b0;
-                end
-                4'h5: begin
-                    /* Line Status Read Only */
-                    if (!WE_I) begin
-                        DAT_O <= {24'b0, LSR};
-                    end
-                    else begin
-                        DAT_O <= 32'bz;
-                    end
-                    ACK_O <= 1'b1;
-                    INT_O <= 1'b0;
-                end
-                4'h6: begin
-                    /* Line Status Read Only */
-                    if (!WE_I) begin
-                        DAT_O <= {24'b0, MSR};
-                    end
-                    else begin
-                        DAT_O <= 32'bz;
-                    end
-                    ACK_O <= 1'b1;
-                    INT_O <= 1'b0;
-                end
-                default: begin
-                    DAT_O <= 32'bz;
-                    ACK_O <= 1'bz;
-                    INT_O <= 1'b0;
-                end
-            endcase
+                endcase
+            end
+            else begin
+                DAT_O <= 32'bz;
+                ACK_O <= 1'bz;
+                INT_O <= 1'b0;
+            end
         end
     end
     
