82ead10bc0a28fb933eb06cf3a54bbbb45dc9f1a
Hanyuan Zhao <hanyuan-z@qq.com>
2024-01-31 19:56:11 +0800

    top: add registers

--- uart_8250.v
+++ uart_8250.v
@@ -10,12 +10,26 @@ module uart_8250 (input CLK_I,             /* 时钟 */
                   input CYC_I,             /* 总线周期信号 */
                   output reg INT_O);       /* 中断信号 */
     
+    reg [7:0] rx_buffer;
+    reg [7:0] tx_buffer;
+    reg [7:0] IER; /* Interrupt Enable Register */
+    reg [7:0] IIR; /* Interrupt ID Register */
+    reg [7:0] FCR; /* FIFO Control Register */
+    reg [7:0] LCR; /* Line Control Register */
+    reg [7:0] MCR; /* Modem Control Register */
+    reg [7:0] LSR; /* Line Status Register */
+    reg [7:0] MSR; /* Modem Status Register */
+    
     always @(posedge CYC_I or negedge RST_I) begin
         if (!RST_I) begin
             
         end
         else begin
-            
+            if (WE_I) begin
+                
+            end
+            else begin
+            end
         end
     end
     
